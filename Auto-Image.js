2ef47dd7ed370e3f97b1ced3ab59a27afd4c2ac3
Marthus Sera <marthus2001@gmail.com>
2025-08-21 03:44:58 -0300

    Fix map

--- Auto-Image.js
+++ Auto-Image.js
@@ -1092,7 +1092,7 @@
     },
 
     findClosestColorFromAllColors: (r, g, b) => {
-      const allColors = Object.values(CONFIG.COLOR_MAP).map(color => [color.rgb.r, color.rgb.g, color.rgb.b]);
+      const allColors = Object.values(CONFIG.COLOR_MAP).map((color) => [color.rgb.r, color.rgb.g, color.rgb.b]);
       return Utils.findClosestPaletteColor(r, g, b, allColors);
     },
 
